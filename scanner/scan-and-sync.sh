3287127bdce0ba8c03f5abb72fb9c8497f5f401b
James Lowther <jamesevlowther@gmail.com>
2023-06-15 13:49:10 -0600

    Add webhook

---
+++ scanner/scan-and-sync.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd $(dirname $0)
+
+source ./.venv/bin/activate
+python3 main.py
+
+echo "Syncing with rsync"
+rsync -a --delete ./scans james@vps.jameslowther.com:~/jeremy-toppables-cracker-count/website
