acf9071e5c43937f4af897b973bfe65a9f409f07
James Lowther <jamesevlowther@gmail.com>
2023-06-18 15:54:04 -0600

    Update website

--- website/src/app.py
+++ website/src/app.py
@@ -10,13 +10,34 @@ def index():
         with open("static/scans/manifest.json", "r") as f:
             manifest = json.loads(f.read())
 
+        version = "v1"
         posts = manifest["posts"]
         updated = manifest["manifest_updated"]
-        total = sum([len(x["scans"]) for x in posts])
+        total = sum([len(x["scans"][version]) for x in posts])
 
     except FileNotFoundError:
+        version=""
         posts = []
         updated = ""
         total = 0
 
-    return render_template("index.html", posts=posts, updated=updated, total=total)
+    return render_template("index.html", version=version, posts=posts, updated=updated, total=total)
+
+@app.route("/v2")
+def version2():
+    try:
+        with open("static/scans/manifest.json", "r") as f:
+            manifest = json.loads(f.read())
+
+        version = "v2"
+        posts = manifest["posts"]
+        updated = manifest["manifest_updated"]
+        total = sum([len(x["scans"][version]) for x in posts])
+
+    except FileNotFoundError:
+        version=""
+        posts = []
+        updated = ""
+        total = 0
+
+    return render_template("index.html", version=version, posts=posts, updated=updated, total=total)
