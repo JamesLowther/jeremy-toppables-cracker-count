702c7d8fd16b3cdeb35c196958848be0640e65e4
James Lowther <jamesevlowther@gmail.com>
2023-06-18 16:12:50 -0600

    Update

--- website/src/app.py
+++ website/src/app.py
@@ -6,38 +6,38 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+    VERSION = "v1"
+
     try:
         with open("static/scans/manifest.json", "r") as f:
             manifest = json.loads(f.read())
 
-        version = "v1"
         posts = manifest["posts"]
         updated = manifest["manifest_updated"]
-        total = sum([len(x["scans"][version]) for x in posts])
+        total = sum([len(x["scans"][VERSION]) for x in posts])
 
     except FileNotFoundError:
-        version=""
         posts = []
         updated = ""
         total = 0
 
-    return render_template("index.html", version=version, posts=posts, updated=updated, total=total)
+    return render_template("index.html", version=VERSION, posts=posts, updated=updated, total=total)
 
 @app.route("/v2")
 def version2():
+    VERSION = "v2"
+
     try:
         with open("static/scans/manifest.json", "r") as f:
             manifest = json.loads(f.read())
 
-        version = "v2"
         posts = manifest["posts"]
         updated = manifest["manifest_updated"]
-        total = sum([len(x["scans"][version]) for x in posts])
+        total = sum([len(x["scans"][VERSION]) for x in posts])
 
     except (FileNotFoundError, KeyError):
-        version=""
         posts = []
         updated = ""
         total = 0
 
-    return render_template("index.html", version=version, posts=posts, updated=updated, total=total)
+    return render_template("index.html", version=VERSION, posts=posts, updated=updated, total=total)
