e3b5d98bc6ec38a9f6f526c0ebf168e79181e422
James Lowther <jamesevlowther@gmail.com>
2023-07-28 21:23:37 -0600

    Update

--- scanner/scan-and-sync.sh
+++ scanner/scan-and-sync.sh
@@ -6,4 +6,4 @@ source ./.venv/bin/activate
 python3 main.py
 
 echo "Syncing with rsync"
-rsync -a --delete ./scans james@vps.jameslowther.com:~/jeremy-toppables-cracker-count/website
+rsync -a --delete ./scans toppables@vps.jameslowther.com:~/jeremy-toppables-cracker-count/website
